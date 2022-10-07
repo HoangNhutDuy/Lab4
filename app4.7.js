a2c691ef45beaaea1d3aedd084f74260b91ddc5b
Hoàng Nhựt Duy <20130238@st.hcmuaf.edu.vn>
2022-10-07 14:37:54 +0700

    update 4.7

--- app4.7.js
+++ app4.7.js
@@ -6,6 +6,8 @@ var random = Math.floor(Math.random() * buttons.length)
 buttons.forEach((button) => {
     buttons[random].addEventListener('click', function () {
         buttons[random].style.opacity = '0';
+        buttons[random].style.pointerEvents = 'none';
+
     })
     button.addEventListener('click', function () {
         if (button != buttons[random]) {
